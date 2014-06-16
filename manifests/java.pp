28903e182dac25ad3df923dd863edbfb5badf536
Toby Clemson <tobyclemson@gmail.com>
2014-06-15 23:24:52 -0400

    Fix package names to meet expectations of the compressed_app provider.

--- manifests/java.pp
+++ manifests/java.pp
@@ -5,7 +5,7 @@
 #   include eclipse::java
 class eclipse::java {
 
-  package { 'Eclipse-Java-Kepler-SR1':
+  package { 'eclipse/Eclipse':
     provider => 'compressed_app',
     source   => 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/kepler/SR1/eclipse-java-kepler-SR1-macosx-cocoa-x86_64.tar.gz',
   }
--- manifests/jee.pp
+++ manifests/jee.pp
@@ -5,7 +5,7 @@
 #   include eclipse::jee
 class eclipse::jee {
 
-  package { 'Eclipse-JEE-Kepler-SR1':
+  package { 'eclipse/Eclipse':
     provider => 'compressed_app',
     source   => 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/kepler/SR1/eclipse-jee-kepler-SR1-macosx-cocoa-x86_64.tar.gz',
   }
